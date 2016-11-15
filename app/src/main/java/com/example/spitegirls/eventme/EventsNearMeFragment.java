bea49d942a90567d2ef8e37fd72a8a529f13880f
BRIAN\Brian <b-arthur@live.ie>
2016-11-15 15:08:11 +0000

    Beep Boop

--- app/src/main/java/com/example/spitegirls/eventme/EventsNearMeFragment.java
+++ app/src/main/java/com/example/spitegirls/eventme/EventsNearMeFragment.java
@@ -18,40 +18,40 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by Clíodhna on 08/11/2016.
  */
 
-public class EventsNearMeFragment extends Fragment {// implements OnMapReadyCallback {
+public class EventsNearMeFragment extends Fragment implements OnMapReadyCallback {
 
-//    public static EventsNearMeFragment newInstance() {
-//        return new EventsNearMeFragment();
-//    }
-//
-//    public EventsNearMeFragment() {
-//
-//    }
+    public static EventsNearMeFragment newInstance() {
+        return new EventsNearMeFragment();
+    }
+
+    public EventsNearMeFragment() {
+
+    }
   private GoogleMap mMap;
 
-//   @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//    }
-//
-//  @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//
-//        // Add a marker in UCD and move the camera
-//        LatLng UCD = new LatLng(53.3053, -6.2207);
-//        LatLng UCDLibrary = new LatLng(53.3068, -6.2230);
-//        String snippet = "Event at 2pm, Law Soc Debate";
-//        Marker UCDLibraryMarker = mMap.addMarker(new MarkerOptions().position(UCDLibrary).title("UCD Library").snippet(snippet));
-//        mMap.addMarker(new MarkerOptions().position(UCD).title("Marker in UCD"));
-//        mMap.setMinZoomPreference(12);
-//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCD, 17));
-//        //mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
-//    }
+   @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+  @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Add a marker in UCD and move the camera
+        LatLng UCD = new LatLng(53.3053, -6.2207);
+        LatLng UCDLibrary = new LatLng(53.3068, -6.2230);
+        String snippet = "Event at 2pm, Law Soc Debate";
+        Marker UCDLibraryMarker = mMap.addMarker(new MarkerOptions().position(UCDLibrary).title("UCD Library").snippet(snippet));
+        mMap.addMarker(new MarkerOptions().position(UCD).title("Marker in UCD"));
+        mMap.setMinZoomPreference(12);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCD, 17));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+}
 
 
     @Override
