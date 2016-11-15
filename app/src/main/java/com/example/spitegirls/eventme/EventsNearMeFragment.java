a2f5b22ad54309af11ed5c860fd640f2143412d7
BRIAN\Brian <b-arthur@live.ie>
2016-11-15 14:17:08 +0000

    We have a map! No Map stuff working yet

--- app/src/main/java/com/example/spitegirls/eventme/EventsNearMeFragment.java
+++ app/src/main/java/com/example/spitegirls/eventme/EventsNearMeFragment.java
@@ -6,25 +6,70 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Clíodhna on 08/11/2016.
  */
 
-public class EventsNearMeFragment extends Fragment {
+public class EventsNearMeFragment extends Fragment {// implements OnMapReadyCallback {
 
-    public static EventsNearMeFragment newInstance() {
-        return new EventsNearMeFragment();
-    }
+//    public static EventsNearMeFragment newInstance() {
+//        return new EventsNearMeFragment();
+//    }
+//
+//    public EventsNearMeFragment() {
+//
+//    }
+  private GoogleMap mMap;
 
-    public EventsNearMeFragment() {
+//   @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//    }
+//
+//  @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in UCD and move the camera
+//        LatLng UCD = new LatLng(53.3053, -6.2207);
+//        LatLng UCDLibrary = new LatLng(53.3068, -6.2230);
+//        String snippet = "Event at 2pm, Law Soc Debate";
+//        Marker UCDLibraryMarker = mMap.addMarker(new MarkerOptions().position(UCDLibrary).title("UCD Library").snippet(snippet));
+//        mMap.addMarker(new MarkerOptions().position(UCD).title("Marker in UCD"));
+//        mMap.setMinZoomPreference(12);
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCD, 17));
+//        //mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+//    }
 
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_events_near_me, container, false);
+        return inflater.inflate(R.layout.activity_maps, container, false);
     }
 
+    @Override
+    public void onDestroyView() {
+
+        Fragment f = (Fragment) getFragmentManager().findFragmentById(R.id.map);
+        if (f != null) {
+            getFragmentManager().beginTransaction().remove(f).commit();
+        }
+
+        super.onDestroyView();
+    }
 }
+
+
