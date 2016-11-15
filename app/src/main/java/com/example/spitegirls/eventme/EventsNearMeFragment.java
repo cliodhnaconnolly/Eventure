4a5ec8fac88b9dad705e38b04eea32cef77e7261
Clíodhna Connolly <cliodhnaconnolly@hotmail.com>
2016-11-15 15:42:21 +0000

    I think its working again

--- app/src/main/java/com/example/spitegirls/eventme/EventsNearMeFragment.java
+++ app/src/main/java/com/example/spitegirls/eventme/EventsNearMeFragment.java
@@ -17,27 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class EventsNearMeFragment extends Fragment implements OnMapReadyCallback {
 
-<<<<<<< HEAD
-    public static EventsNearMeFragment newInstance() {
-        return new EventsNearMeFragment();
-    }
-
-    public EventsNearMeFragment() {
-
-    }
-  private GoogleMap mMap;
-
-   @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-=======
-//    public static EventsNearMeFragment newInstance() {
+    //    public static EventsNearMeFragment newInstance() {
 //        return new EventsNearMeFragment();
 //    }
 //
@@ -46,31 +26,12 @@ public class EventsNearMeFragment extends Fragment implements OnMapReadyCallback
 //    }
     private GoogleMap mMap;
     private MapView mapView;
-<<<<<<< HEAD
 
     @Override
-=======
-//
-//   @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//    }
-//
->>>>>>> fcc3de21921976f95e771d1b6f62db6b6d4a86d3
-  @Override
->>>>>>> 88e6a99bcb499f7c23fb333ef52ff4f3936f27f8
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-<<<<<<< HEAD
-        //Add a marker in UCD and move the camera
-=======
         // Add a marker in UCD and move the camera
->>>>>>> fcc3de21921976f95e771d1b6f62db6b6d4a86d3
         LatLng UCD = new LatLng(53.3053, -6.2207);
         LatLng UCDLibrary = new LatLng(53.3068, -6.2230);
         String snippet = "Event at 2pm, Law Soc Debate";
@@ -78,13 +39,8 @@ public class EventsNearMeFragment extends Fragment implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(UCD).title("Marker in UCD"));
         mMap.setMinZoomPreference(12);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCD, 17));
-<<<<<<< HEAD
-        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
-}
-=======
         //mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
     }
->>>>>>> fcc3de21921976f95e771d1b6f62db6b6d4a86d3
 
 
     @Override
@@ -114,5 +70,3 @@ public class EventsNearMeFragment extends Fragment implements OnMapReadyCallback
         mapView.onDestroy();
     }
 }
-
-
